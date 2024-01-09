a9b1a4b9f9d37befd98ae85d6383ad2b63869336
Muhammad Naufal Arkan <naufalarkan@students.undip.ac.id>
2024-01-09 22:50:23 +0700

    add breakpoint

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,6 +17,14 @@ export default withMT({
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
+    screens: {
+      xs: { max: "320px" },
+      sm: { max: "576px" },
+      md: { max: "768px" },
+      lg: { max: "992px" },
+      xl: { max: "1200px" },
+      "2xl": { max: "1536px" },
+    },
   },
   plugins: [],
 });
