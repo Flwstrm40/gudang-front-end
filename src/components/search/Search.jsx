a2626b70008f247b085f671899f278634780c870
Muhammad Naufal Arkan <naufalarkan@students.undip.ac.id>
2024-01-10 23:18:36 +0700

    slight update search

--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -1,25 +1,47 @@
 'use client';
 import { Input, Button } from "@material-tailwind/react";
+import { useState, useEffect } from "react";
 
 const Search = () => {
-    return ( 
+  const [searchText, setSearchText] = useState("");
+
+  const handleSearch = () => {
+    console.log(searchText);
+  }
+
+  useEffect(() => {
+    // Add event listener ketika user menekan tombol Enter
+    const handleKeyPress = (e) => {
+      if (e.key === 'Enter' && searchText !== "" && document.activeElement.id === "search-input") {
+        // If Enter key is pressed, call the handleLogin function
+        handleSearch(e);
+      }
+    };
+    document.addEventListener('keydown', handleKeyPress);
+    return () => {
+      document.removeEventListener('keydown', handleKeyPress);
+    };
+  }, [searchText]);
+
+  return ( 
       <div className="w-[70%]">
         <div className="relative flex w-full gap-2 md:w-max">
           <Input
+            id="search-input"
             type="text"
             color="blue"
             label="Cari di sini..."
             className="pr-20"
-            // containerProps={{
-            //   className: "min-w-[288px]",
-            // }}
+            value={searchText}
+            onChange={(e) => setSearchText(e.target.value)}
           />
           <Button
             size="sm"
             color="blue"
             className="!absolute right-1 top-1 rounded"
+            onClick={handleSearch}
           >
-            Search
+            Cari
           </Button>
         </div>
       </div>
