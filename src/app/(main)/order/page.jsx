44b23ffec58ef205a0dc720f505225e9397d1dbd
Muhammad Naufal Arkan <naufalarkan@students.undip.ac.id>
2024-01-12 23:07:08 +0700

    add order page

---
+++ src/app/(main)/order/page.jsx
@@ -0,0 +1,16 @@
+import OrderCard from "@/components/order/OrderCard";
+
+export const metadata = {
+  title: 'Pesanan',
+  description: 'Pesanan page',
+}
+
+const Order = () => {
+    return ( 
+      <div>
+        <OrderCard/>
+      </div>
+    );
+  }
+  
+  export default Order;
