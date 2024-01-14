cd924569c6d77a888c7043f2fd8604427ea08d31
Muhammad Naufal Arkan <naufalarkan@students.undip.ac.id>
2024-01-14 23:32:07 +0700

    clean code from comment

--- src/components/inventory/InventoryCard.jsx
+++ src/components/inventory/InventoryCard.jsx
@@ -1,25 +1,12 @@
 'use client';
 
-import { 
-  Card,
-  CardBody,
-} from "@material-tailwind/react";
-import Search from "../search/Search";
-import SortBy from "../sortBy/SortBy";
 import TableInventory from "./TableInventory";
-import DialAddProduk from "./DialAddProduk";
-import Pagination from "../pagination/Pagination";
-import { Toaster, toast } from "sonner";
-
 const InventoryCard = () => {
     return ( 
       <div>
         <div className="text-lg">
           <TableInventory />
         </div>
-        {/* <div>
-          <DialAddProduk/>
-        </div> */}
       </div>
     );
   }
