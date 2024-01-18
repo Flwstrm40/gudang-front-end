1bf8eb31c7716728231f5fdf1aa4ae7ac5714a9c
Muhammad Naufal Arkan <naufalarkan@students.undip.ac.id>
2024-01-18 09:57:46 +0700

    feat: make page acc mngmnt and add fitur reset Password

--- src/components/accountManagement/AccountManagementCard.jsx
+++ src/components/accountManagement/AccountManagementCard.jsx
@@ -1,7 +1,89 @@
+'use client';
+
+import { Button, Card } from "@material-tailwind/react";
+import useSWR, {mutate} from "swr";
+import axios from "axios";
+import { KeyIcon } from "@heroicons/react/24/solid";
+import { Toaster, toast } from "sonner";
+import ModalKonfirmasiReset from "./ModalKonfirmasiReset";
+
 const AccountManagementCard = () => {
+  const {data: dataKepalaGudang, error} = useSWR(`http://localhost:5050/user/kepala-gudang`, async (url) => {
+    const response = await axios.get(url);
+    return response.data[0];
+  });
+
+  const {data: dataAdmin, errorAdmin} = useSWR(`http://localhost:5050/user/admin`, async (url) => {
+    const response = await axios.get(url);
+    return response.data.admins;
+  });
+
+  // console.log(dataKepalaGudang);
+  // console.log(dataAdmin);
+
     return ( 
-      <div className="text-xl">
-        AccountManagementCard Page Ini
+      <div className="text-black text-sm">
+        {/* Kepala Gudang */}
+        <div className="text-lg font-semibold mb-5">
+          Kepala Gudang
+        </div>
+        <Card shadow={false} className="border p-4 rounded-md text-sm text-black">
+          <div className="flex gap-5 justify-between items-center">
+            <div className="flex gap-5">
+              <div>
+                <div>Username </div>
+                <div>Display Name</div>
+              </div>
+              <div className="flex gap-1">
+                <div>
+                  <div>:</div>
+                  <div>:</div>
+                </div>
+                <div>
+                  <div className="font-semibold">{dataKepalaGudang?.username}</div>
+                  <div className="font-semibold">{dataKepalaGudang?.displayName}</div>
+                </div>
+              </div>
+            </div>
+
+            <div>
+              <ModalKonfirmasiReset id={dataKepalaGudang?.id} username={dataKepalaGudang?.username} />
+            </div>
+          </div>
+        </Card>
+
+        {/* Admin */}
+        <div className="text-lg font-semibold mt-10 mb-5">
+          Admin
+        </div>
+        {dataAdmin?.map(({id, username, displayName}) => (
+          <Card key={id} shadow={false} className="mb-3 border p-4 rounded-md text-sm text-black">
+            <div className="flex gap-5 justify-between items-center">
+              <div className="flex gap-5">
+                <div>
+                  <div>Username </div>
+                  <div>Display Name</div>
+                </div>
+                <div className="flex gap-1">
+                  <div>
+                    <div>:</div>
+                    <div>:</div>
+                  </div>
+                  <div>
+                    <div className="font-semibold">{username}</div>
+                    <div className="font-semibold">{displayName? displayName: "-"}</div>
+                  </div>
+                </div>
+              </div>
+
+              <div>
+               <ModalKonfirmasiReset id={id} username={username} />
+              </div>
+            </div>
+          </Card>
+        ))}
+
+
       </div>
     );
   }
---
+++ src/components/accountManagement/ModalKonfirmasiReset.jsx
@@ -0,0 +1,88 @@
+import React from "react";
+import {
+  Button,
+  Dialog,
+  DialogHeader,
+  DialogBody,
+  DialogFooter,
+} from "@material-tailwind/react";
+import { 
+    CheckIcon,
+    KeyIcon
+} from "@heroicons/react/24/solid";
+import axios from "axios";
+import { Toaster, toast } from 'sonner'
+import { parseCookies } from "nookies";
+ 
+export default function ModalKonfirmasiReset({id, username}) {
+  const [open, setOpen] = React.useState(false);
+  const cookies = parseCookies();
+  const role = cookies.role;
+
+  const handleOpen = () => setOpen(!open);
+ 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+        const resetPasswordRes = await axios.put(`http://localhost:5050/user/reset-password/${id}`, {
+          password: "admin"
+        });
+    
+        if (resetPasswordRes.status === 200) {
+          toast.success("Password berhasil direset.");
+          handleOpen();
+        } else {
+          toast.error("Gagal mereset password.");
+        }
+      } catch (error) {
+        toast.error("Terjadi kesalahan, gagal mereset password.");
+        console.error(error);
+      }
+  };
+  
+
+
+  return (
+    <>
+     <Button color="red" variant="text" className="py-2 px-4" onClick={handleOpen}>
+            <KeyIcon className="h-5 w-5"/>
+        </Button>
+      <Dialog
+        open={open}
+        handler={handleOpen}
+        animate={{
+          mount: { scale: 1, y: 0 },
+          unmount: { scale: 0.9, y: -100 },
+        }}
+      >
+        <DialogHeader>
+            <div className="flex flex-col">
+                <div>
+                    Konfirmasi Reset
+                </div>
+            </div>
+        </DialogHeader>
+        <DialogBody>
+            Password 
+            <span className="font-semibold">{` ${username}`} </span>
+            akan direset menjadi "
+            <span className="font-semibold">admin</span>
+            ". Apakah anda yakin?
+        </DialogBody>
+        <DialogFooter>
+          <Button
+            variant="text"
+            color="red"
+            onClick={handleOpen}
+            className="mr-1"
+          >
+            <span>Cancel</span>
+          </Button>
+          <Button variant="gradient" color="green" onClick={handleSubmit}>
+            <span>Konfirmasi</span>
+          </Button>
+        </DialogFooter>
+      </Dialog>
+    </>
+  );
+}
