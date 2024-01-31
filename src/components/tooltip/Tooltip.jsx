13709e743cb0447018ae6d616e527c181487792c
Muhammad Naufal Arkan <naufalarkan@students.undip.ac.id>
2024-01-31 23:14:39 +0700

    change ui

--- src/components/tooltip/Tooltip.jsx
+++ src/components/tooltip/Tooltip.jsx
@@ -22,7 +22,7 @@ export function TooltipIcon({message}) {
         viewBox="0 0 24 24"
         stroke="currentColor"
         strokeWidth={2}
-        className="h-5 w-5 cursor-pointer text-blue-gray-500"
+        className="h-5 w-5 cursor-pointer text-blue-gray-500 hover:text-blue-800"
       >
         <path
           strokeLinecap="round"
