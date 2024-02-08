9a491ecca9253d839b8b70e48241dac6fdd7575e
Muhammad Naufal Arkan <naufalarkan@students.undip.ac.id>
2024-02-08 22:03:43 +0700

    add Dial toko for kepala gudang

--- src/components/stockTransfer/DialFormTransfer.jsx
+++ src/components/stockTransfer/DialFormTransfer.jsx
@@ -11,11 +11,16 @@ import {
     HomeIcon,
     CogIcon,
     Square3Stack3DIcon,
+    BuildingStorefrontIcon,
     ArrowsRightLeftIcon,
   } from "@heroicons/react/24/outline";
   import Link from "next/link";
+  import { parseCookies } from "nookies";
    
   export default function DialFormTransfer() {
+    const cookies = parseCookies();
+    const role = cookies.role;
+
     return (
       <div className="w-full">
         <div>
@@ -26,6 +31,16 @@ import {
               </IconButton>
             </SpeedDialHandler>
             <SpeedDialContent>
+              {role === "kepala gudang" && (
+                 <Link href="/stockTransfer/store">
+                  <SpeedDialAction className="h-16 w-16 hover:bg-blue-500 hover:text-white">
+                    <BuildingStorefrontIcon className="h-5 w-5" />
+                    <div color="blue-gray" className="text-xs font-normal">
+                      Toko
+                    </div>
+                  </SpeedDialAction>
+                </Link>
+              )}
               <Link href="/stockTransfer/formTransfer">
                 <SpeedDialAction className="h-16 w-16 hover:bg-blue-500 hover:text-white">
                   <ArrowsRightLeftIcon className="h-5 w-5" />
